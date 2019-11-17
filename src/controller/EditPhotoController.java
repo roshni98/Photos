2b79f9b3f24bfaefa6876bbf60b09c043d2f28de
Amulya <amulya.mummaneni@gmail.com>
2019-11-17 16:25:33 -0500

    made changes to editphotocontroller

--- src/controller/EditPhotoController.java
+++ src/controller/EditPhotoController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.fxml.FXML;
+import model.Photo;
+
 public class EditPhotoController {
+
+    @FXML
+    TextField dateField;
+
+    public void start(Stage primaryStage, Photo photo){
+        dateField.setText(photo.date());
+    }
 }
